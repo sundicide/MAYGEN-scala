366d4eaeabe59f3fd0754991897521aff619b8b2
sundicide <kyusuen@gmail.com>
2022-05-26 20:53:00 +0900

    complete distribute func

--- src/main/scala/MAYGEN/HydrogenDistributor.scala
+++ src/main/scala/MAYGEN/HydrogenDistributor.scala
@@ -25,18 +25,102 @@ object HydrogenDistributor {
     )
   }
 
-  def distribute(formula: String): Unit = {
+  def distribute(formula: String): List[List[Int]] = {
     val valences = Utils.getValences(formula)
-    val atoms = Utils.getOccurences(formula)
-
+    val occurrences = Utils.getOccurences(formula)
     val hydrogenCount = Utils.getHydrogensCount(formula)
-    for {
-      i <- valences
-//      ii <- Math.min(i, hydrogenCount)
-    } {
-      print(i)
-//      if ()
+
+    /**
+     * 모든 경우의 수에 해당하는 배열을 생성한다.
+     * @param arrSize 생성될 배열의 사이즈
+     * @param maxValue 각 요소에 할당할 수 있는 최대 값
+     * @return 생성된 배열 List
+     */
+    def makeArray(arrSize: Int, maxValue: Int) = {
+      val initList: List[List[Int]] = (0 to maxValue).toList map((d: Int) =>  List(d))
+
+      def iter(p: List[List[Int]]) = {
+        for {
+          pi <- p
+          i <- 0 to maxValue
+        } yield (i :: pi)
+      }
+
+      def doIter(count: Int, currentList: List[List[Int]]): List[List[Int]] = {
+        if (count < arrSize - 1) doIter(count + 1, iter(currentList))
+        else currentList
+      }
+      doIter(0, initList)
+    }
+    /**
+     *
+     * @param arrSize 배열 길이
+     * @param maxValue 배열내 원소의 최대 값
+     * @param totalValue 배열 내 모든 원소의 총합 기준
+     *              예) total=2 라면 배열 내 모든 원소의 합이 2여야 한다.
+     * @return
+     */
+    def filterByTotalValue(arrSize: Int, maxValue: Int, totalValue: Int): List[List[Int]] = {
+      makeArray(arrSize, maxValue).filter((d: List[Int]) => d.sum <= totalValue)
+    }
+    /**
+     * 맨 뒤에서 부터 값이 채워진 배열 리턴
+     * @param arrSize 배열 길이
+     * @param maxValue 배열내 원소의 최대 값
+     * @param totalValue 배열 내 모든 원소의 총합 기준
+     *              예) total=2 라면 배열 내 모든 원소의 합이 2여야 한다.
+     * @return
+     */
+    def sortArray(arrSize: Int, maxValue: Int, totalValue: Int) = {
+      filterByTotalValue(arrSize, maxValue, totalValue).map(_.sorted)
+        .distinct
     }
 
+    val result = (valences zip occurrences).init
+
+    result
+      .map({
+      case (valence: Int, occurs: Int) => sortArray(occurs, valence, hydrogenCount)
+      })
+      .reduceLeft((a, b) => {
+        for {
+          aa <- a
+          bb <- b
+        } yield aa.concat(bb)
+      })
+      .filter(d => d.sum == hydrogenCount)
+  }
+
+  def newDegrees(formula: String): List[List[Int]] = {
+    val valences = Utils.getValences(formula)
+    val occurrences = Utils.getOccurences(formula)
+
+    val result = (valences zip occurrences).init
+
+    val distributed = distribute(formula)
+
+    val tt = result.toList.foldLeft(List(0))((acc, curr) => {
+      def append(targetArr: List[Int], idx: Int): List[Int] = {
+        if (idx == 0) targetArr
+        else append((curr._1 + 1) :: targetArr, idx - 1)
+      }
+      append(acc, curr._2)
+    }).init.reverse
+
+    distributed.map(d => {
+      d.zipWithIndex.map(dd => tt(dd._2) - dd._1)
+    })
+  }
+
+  def newPartition(formula: String): List[List[Int]] = {
+    newDegrees(formula).map(f => {
+      f.foldLeft(Nil: List[(Int, Int)])((accu, curr) => {
+        if (accu.isEmpty) List((curr, 1))
+        else {
+          if (curr == accu.head._1) (curr, accu.head._2 + 1) :: accu.tail
+          else (curr, 1) :: accu
+        }
+      }).reverse.map(d => d._2)
+    })
   }
 }
--- src/test/scala/HydrogenDistributorTest.scala
+++ src/test/scala/HydrogenDistributorTest.scala
@@ -1,92 +1,35 @@
-import MAYGEN.Utils
+import MAYGEN.{HydrogenDistributor, Utils}
 import org.junit.Test
 import org.junit.Assert._
 
 class HydrogenDistributorTest {
   trait TestSets {
     val testSmiles = "c2O6H2"
+    val testSmiles2 = "C6H6"
     val normalizedSmiles = Utils.normalizeFormula(testSmiles)
     val normalizedSmilesAtoms = Array("C2", "O6", "H2")
 
     val abnormalSmiles = "asdqwe"
     val normalizedAbnormalSmiles = Utils.normalizeFormula(abnormalSmiles)
   }
-  @Test def hydrogenDistributePrepareTest(): Unit = {
-    /**
-     * 모든 경우의 수에 해당하는 배열을 생성한다.
-     * @param arrSize 생성될 배열의 사이즈
-     * @param maxValue 각 요소에 할당할 수 있는 최대 값
-     * @return 생성된 배열 List
-     */
-    def makeArray(arrSize: Int, maxValue: Int) = {
-      val initList: List[List[Int]] = (0 to maxValue).toList map((d: Int) =>  List(d))
 
-      def iter(p: List[List[Int]]) = {
-        for {
-          pi <- p
-          i <- 0 to maxValue
-        } yield (i :: pi)
-      }
-
-      def doIter(count: Int, currentList: List[List[Int]]): List[List[Int]] = {
-        if (count < arrSize - 1) doIter(count + 1, iter(currentList))
-        else currentList
-      }
-      doIter(0, initList)
+  @Test def `newDegrees`() : Unit = {
+    new TestSets {
+      val result = HydrogenDistributor.newDegrees(normalizedSmiles)
+      assertEquals(
+        List(List(4, 4, 2, 2, 2, 2, 1, 1), List(4, 3, 2, 2, 2, 2, 2, 1), List(4, 2, 2, 2, 2, 2, 2, 2), List(3, 3, 2, 2, 2, 2, 2, 2)),
+        result
+      )
     }
-
-    assertEquals(
-      List(List(0, 0), List(1, 0), List(2, 0), List(0, 1), List(1, 1), List(2, 1), List(0, 2), List(1, 2), List(2, 2)),
-      makeArray(2, 2)
-    )
-
-    /**
-     *
-     * @param arrSize 배열 길이
-     * @param maxValue 배열내 원소의 최대 값
-     * @param totalValue 배열 내 모든 원소의 총합 기준
-     *              예) total=2 라면 배열 내 모든 원소의 합이 2여야 한다.
-     * @return
-     */
-    def filterByTotalValue(arrSize: Int, maxValue: Int, totalValue: Int): List[List[Int]] = {
-      makeArray(arrSize, maxValue).filter((d: List[Int]) => d.sum <= totalValue)
-    }
-
-    /**
-     * 맨 뒤에서 부터 값이 채워진 배열 리턴
-     * @param arrSize 배열 길이
-     * @param maxValue 배열내 원소의 최대 값
-     * @param totalValue 배열 내 모든 원소의 총합 기준
-     *              예) total=2 라면 배열 내 모든 원소의 합이 2여야 한다.
-     * @return
-     */
-    def sortArray(arrSize: Int, maxValue: Int, totalValue: Int) = {
-      filterByTotalValue(arrSize, maxValue, totalValue).map(_.sorted)
-        .distinct
-    }
-
-    val total = 6
-    val targetList = List(
-      List(6, 3, total)
-    )
-
-    val mapped = targetList.map(d => {
-      sortArray(d(0), d(1), d(2))
-    })
-
-    val temp = mapped.reduceLeft((a, b) => {
-      for {
-        aa <- a
-        bb <- b
-      } yield aa.concat(bb)
-    })
-    val result = temp.filter(d => d.sum == total)
-    assertEquals(
-      List(List(0, 0, 0, 0, 3, 3), List(0, 0, 0, 1, 2, 3), List(0, 0, 0, 2, 2, 2), List(0, 0, 1, 1, 1, 3), List(0, 0, 1, 1, 2, 2), List(0, 1, 1, 1, 1, 2), List(1, 1, 1, 1, 1, 1)),
-      result
-    )
   }
 
-  @Test def `zipTest`() : Unit = {
+  @Test def `newPartition`() : Unit = {
+    new TestSets {
+      val result = HydrogenDistributor.newPartition(normalizedSmiles)
+      assertEquals(
+        List(List(2, 4, 2), List(1, 1, 5, 1), List(1, 7), List(2, 6)),
+        result
+      )
+    }
   }
 }
